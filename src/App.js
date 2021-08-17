60eced721c48c045980351952e137fe1f5908b74
jonathanludena <jonathanludenai@gmail.com>
2021-08-16 22:02:38 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import About from "./components/About";
+import Users from "./components/Users";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container p-4">
+        <Switch>
+          <Route exact path="/" component={Users} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus,
+        dicta. Dignissimos dolor eius odit porro dicta doloremque tempore id
+        consequuntur itaque repudiandae quasi labore debitis neque aliquid ab
+        laborum maiores quis ducimus, sapiente nobis. Repudiandae magni sit
+        alias delectus doloremque, numquam possimus, voluptatibus, odit
+        explicabo sint dolorem? Enim animi, provident commodi exercitationem
+        ipsam cumque sapiente quidem excepturi beatae, qui asperiores
+        consequuntur nesciunt ipsum corporis nemo? Architecto ipsam quibusdam
+        ipsa recusandae quidem quo amet voluptatibus corporis expedita
+        consequatur exercitationem cupiditate repellendus error tenetur, atque
+        similique, commodi asperiores pariatur, provident labore ad sunt
+        voluptates modi. Quas odit doloremque atque natus praesentium? Ducimus
+        harum, consequatur, sunt qui quam ad, obcaecati iure cumque vero
+        quisquam beatae magnam autem tempore. Culpa iusto sit incidunt quod
+        esse? Eveniet architecto dolorem distinctio error fuga rerum qui
+        repellat iusto amet labore, autem ea quo nihil vel? Quibusdam temporibus
+        molestiae expedita, quos quo sunt harum. Quos ipsum vel fuga! In minima
+        et repudiandae quas vero cumque, excepturi veritatis, nesciunt quis
+        reiciendis tenetur ullam? Quia repellat, vel explicabo iste quae
+        provident quos voluptates deleniti cumque! Tempore pariatur expedita
+        blanditiis repellendus voluptatum magni. Ex beatae maiores rerum ea
+        porro, autem a nobis soluta, dicta doloremque perspiciatis suscipit
+        expedita. Minus tenetur ipsa sit quidem vitae natus, atque impedit, cum,
+        quis obcaecati perspiciatis molestias fugit provident repellendus
+        mollitia quae ab deleniti porro adipisci. Praesentium deleniti corrupti
+        in dolore aspernatur quis minus dolor ut quia illum quibusdam cumque
+        inventore, dolorum cupiditate distinctio accusantium sapiente,
+        dignissimos pariatur ab delectus iste alias! Asperiores magni odit
+        dignissimos unde possimus officiis dolore dolorem porro sit nam, vitae,
+        est ratione veniam nulla tempora? Enim nulla animi illo eos veniam
+        aliquid eius, distinctio rerum id odio earum corrupti tempora sunt
+        voluptatibus labore amet. At, quas natus fugit perspiciatis adipisci
+        aliquid molestiae repellat. Voluptas sed pariatur porro sequi corrupti
+        suscipit delectus.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit vero, ad
+        architecto fuga accusantium adipisci asperiores nesciunt praesentium
+        iure, deleniti reiciendis cupiditate. Rem quod sapiente placeat. Tempora
+        assumenda alias enim veniam possimus sint quod, cumque debitis, vitae
+        facere optio minima deserunt ullam ut sed! Ipsa, quibusdam assumenda
+        quos atque facere nemo amet iste quia accusantium consequatur id magni
+        nihil totam quis dolore cupiditate. Voluptatibus fugit, aperiam nemo
+        eveniet repellendus doloribus quia veniam molestiae hic, debitis facere
+        quibusdam architecto similique tempora vel necessitatibus. Amet
+        distinctio cupiditate architecto tempore laudantium ea, maiores sapiente
+        eveniet vitae voluptatum similique incidunt, ullam, id cumque
+        consequatur?
+      </p>
+    </>
+  );
+};
+
+export default About;
